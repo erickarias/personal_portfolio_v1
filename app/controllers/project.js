ec53a12d20c91ae82003ed17792c8ed1f17b5dc9
Erick Arias <erick.arias@wrapports.com>
2014-08-11 10:01:41 -0500

    added arrows to help navigate through projects

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -2,5 +2,17 @@ export default Ember.ObjectController.extend({
   hasRelated: function() {
     return !!this.get('items') === true;
   }.property('items'),
+
+  nextProject: function(){
+    var id = this.get('id');
+    id ++;
+    return id;
+  }.property('id'),
+
+  prevProject: function(){
+    var id = this.get('id');
+    id --;
+    return id;
+  }.property('id'),
 });
 
