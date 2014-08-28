bfca3901bbff3f5c3fdb486bff87a4fdf1af6434
Erick Arias <erick.arias@wrapports.com>
2014-08-28 15:24:54 -0500

    removed arrows to test out hasRelated

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -1,16 +1,4 @@
 export default Ember.ObjectController.extend({
-  nextProject: function(){
-    var id = this.get('id');
-    id ++;
-    return id;
-  }.property('id'),
-
-  prevProject: function(){
-    var id = this.get('id');
-    id --;
-    return id;
-  }.property('id'),
-
   hasRelated: function() {
     return !!this.get('items') === true;
   }.property('items'),
