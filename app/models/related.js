59727b739e17d4b0e68081fbb85f8500718c16ca
Erick Arias <erick.arias@wrapports.com>
2014-08-28 14:53:15 -0500

    reverted back changes to highschool cube

--- app/models/related.js
+++ app/models/related.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 var Related = DS.Model.extend({
   title: DS.attr('string'),
   image: DS.attr('string'),
-  project: DS.belongsTo('project', {async: true})
+  project: DS.belongsTo('project')
 });
 
 
@@ -24,26 +24,6 @@ Related.reopenClass({
     { id: 103,
       title: 'To Do Homepage',
       image: 'assets/images/wires/todo-home.png'
-    },
-    { id: 104,
-      title: 'Season Pass',
-      image: 'assets/images/wires/hscnews_v1.jpg'
-    },
-    { id: 105,
-      title: 'Module Guide 1',
-      image: 'assets/images/wires/hscnews_guide1.jpg'
-    },
-    { id: 106,
-      title: 'Module Guide 2',
-      image: 'assets/images/wires/hscnews_guide2.jpg'
-    },
-    { id: 107,
-      title: 'Module Guide 3',
-      image: 'assets/images/wires/hscnews_guide3.jpg'
-    },
-    { id: 108,
-      title: 'Module Guide 4',
-      image: 'assets/images/wires/hscnews_guide4.jpg'
     }
   ]
 });
