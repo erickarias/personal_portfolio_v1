2637e00454c61369b0d632aab378ecf4c3857174
Erick Arias <erick.arias@wrapports.com>
2014-09-26 14:58:06 -0500

    added descriptions to images

--- app/models/project.js
+++ app/models/project.js
@@ -18,7 +18,7 @@ Project.reopenClass({
       description: 'The concept my team developed for the Chicago.com was a “To do” recommend engine. We envisioned a feed that was custom tailored to your preferences. However, the format of Chicago.com changed and we moved our original concept to todo.chicago.com. We then ended up tweaking the design to capture more of the millennial demographic. I lead the team effort of the visual / UI design and branding. I also led front-end development efforts once our external development team rolled off the project. Main front-end contributions; EmberJS, SASS, Handlebars templates; experience with unit tests. ',
       thumbnail: 'assets/images/thumbnails/to-do-chicago.png',
       url: 'http://todo.chicago.com',
-      related:[100]
+      related:[100, 101, 102, 103, 104, 105]
     },
 
     { id: 2,
@@ -27,7 +27,7 @@ Project.reopenClass({
       description: 'My team was asked to create a website that aggregated articles from all Wrapports publications. I worked with our UX architect on wireframes and prototypes. I was in charge of all the visual / UI design and branding for this project. Front-end development was a collaboration between myself and the developers at DevMynd. Main front-end contributions; EmberJS, SASS, Handlebars templates; experience with unit tests.',
       thumbnail: 'assets/images/thumbnails/chicago-portal.png',
       url: 'http://chicago.com',
-      related:[200]
+      related:[200, 201, 202]
     },
 
     { id: 3,
@@ -36,7 +36,7 @@ Project.reopenClass({
       description: 'While with Chicago.com we developed a platform that allowed customers to purchase @chicago.com email addresses. I worked extensively with our UX architect on developing a seamless experience for searching and buying personalized email addresses. I produced all of the visual designs for the interface and worked with an offshore team to develop the front-end of this Ruby on Rails. ',
       thumbnail: 'assets/images/thumbnails/@chicago.png',
       url: 'http://email.chicago.com',
-      related:[300]
+      related:[300, 301, 302, 303]
     },
 
     { id: 4,
@@ -45,7 +45,7 @@ Project.reopenClass({
       description: 'The Aggrego team was tasked with developing a new platform for all suburban community publications of the Pioneer Press. When the platform launched, we had over 40 websites using our system called “Hermes”. I was responsible for designing responsive layouts and coding templates and feature sets. I also had exposure to Ruby, VIM and unit testing. All sites were redesigned a year after launch under my supervision. ',
       thumbnail: 'assets/images/thumbnails/naperville-sun.png',
       url: 'http://napervillesun.suntimes.com',
-      related:[400]
+      related:[400, 401, 402, 403, 404]
     },
 
     { id: 5,
@@ -54,7 +54,7 @@ Project.reopenClass({
       description: 'Worked with a great team as designer and front-end developer to completely redesign and rebuild the Vivid Seats site. I was responsible for designing and building layouts, implementing JS features, crossbrowser testing and producing widgets for affiliate websites. ',
       thumbnail: 'assets/images/thumbnails/vivid-seats.png',
       url: 'http://vividseats.com',
-      related:[500]
+      related:[500, 501, 502]
     },
 
 
@@ -64,7 +64,7 @@ Project.reopenClass({
       description: 'I had the opportunity to launch a new high school sports product for the Chicago Sun-Times. The site covered all Chicagoland area high schools in every ISHA sport. I was responsible for UI design, building a style guide, designing new advertising opportunities, and coding widgets to be implemented via the third party platform, Sports Ngin.',
       thumbnail: 'assets/images/thumbnails/HSC-news.png',
       items: true,
-      related:[600]
+      related:[600, 601, 602]
     }
   ]
 });
--- app/models/related.js
+++ app/models/related.js
@@ -1,44 +1,108 @@
 import DS from 'ember-data';
 
 var Related = DS.Model.extend({
-  title: DS.attr('string'),
   image: DS.attr('string'),
+  caption: DS.attr('string'),
   project: DS.belongsTo('project')
 });
 
 Related.reopenClass({
   FIXTURES: [
     { id: 100,
-      title: 'Chicago.com Sitemap',
-      image: 'assets/images/wires/chicago-sitemap.png'
+      image: 'assets/images/screenshots/todo-wires.jpg',
+      caption: "Initial mobile wireframes based on business requirement we gathered."
+    },
+    { id: 101,
+      image: 'assets/images/screenshots/todo-v1-cards.jpg',
+      caption: "Discovery and ideation process for potential card types."
+    },
+    { id: 102,
+      image: 'assets/images/screenshots/todo-v1-home-desktop.jpg',
+      caption: "Initial mockups for card pattern homepage on desktop."
+    },
+    { id: 103,
+      image: 'assets/images/screenshots/todo-v1-home-mobile.jpg',
+      caption: "Mobile flow of chicago.com concept. Responsive layout."
+    },
+    { id: 104,
+      image: 'assets/images/screenshots/todo-v2-home-desktop.jpg',
+      caption: "Rebranded as To Do Chicago. Focus on featured content and younger demographic."
+    },
+    { id: 105,
+      image: 'assets/images/screenshots/todo-v2-home-mobile.jpg',
+      caption: "Rebranded mobile homepage. Responsive layout."
     },
     { id: 200,
-      title: 'Chicago.com pages',
-      image: 'assets/images/wires/chicago-pages.png'
+      image: 'assets/images/screenshots/chicago-wires.jpg',
+      caption: "Mobile wireframes for user flow."
+    },
+    { id: 201,
+      image: 'assets/images/screenshots/chicago-mobile.jpg',
+      caption: "Mobile user flow. Homepage, Section, Article"
+    },
+    { id: 202,
+      image: 'assets/images/screenshots/chicago-home-desktop.jpg',
+      caption: "Homepage layout for desktop."
     },
     { id: 300,
-      title: 'Chicago.com content types',
-      image: 'assets/images/wires/chicago-contenttypes.png'
+      image: 'assets/images/screenshots/email-v2-home-desktop.jpg',
+      caption: "Landing page experience allows users to search there name for an @chicago email or domain name. When the user scrolls down they are shown testimonials and marketing copy"
+    },
+    { id: 301,
+      image: 'assets/images/screenshots/email-v1-vanity.jpg',
+      caption: "We marketed the email product as a way for Chicagoans to rep their city."
+    },
+    { id: 302,
+      image: 'assets/images/screenshots/email-v2-results-mobile.jpg',
+      caption: "Fully responsive site allowed users to an purchase email address with a few simple clicks."
+    },
+    { id: 303,
+      image: 'assets/images/screenshots/email-cart.jpg',
+      caption: "We mapped out and designed a simple purchase process that is fully responsive."
     },
     { id: 400,
-      title: ' ',
-      image: 'assets/images/wires/hscnews_v1.jpg'
+      image:  'assets/images/screenshots/hermes-icons.jpg' ,
+      caption: "Designed and implemented a complete set of custom icons for the Hermes platform."
+    },
+    { id: 401,
+      image:  'assets/images/screenshots/hermes-v1-home.jpg' ,
+      caption: "Fully responsive layouts used across 44 suburban publications."
+    },
+    { id: 402,
+      image:  'assets/images/screenshots/hermes-v2-home.jpg' ,
+      caption: "Redesigned homepage layout. Client requested a more folksy look for suburban publications."
+    },
+    { id: 403,
+      image:  'assets/images/screenshots/hermes-v2-home-mobile.jpg',
+      caption: "Responsive mobile layouts for redesign."
+    },
+    { id: 404,
+      image:  'assets/images/screenshots/hermes-v2-nav.jpg' ,
+      caption: "Responsive navigation for redesign. Offered color options"
     },
     { id: 500,
-      title: ' ',
-      image: 'assets/images/wires/hscnews_guide1.jpg'
+      image: 'assets/images/screenshots/vs-home.jpg',
+      caption: "Completed redesign of Vivid Seats site and branding. Homepage layout features top performing events and top level navigation."
+    },
+    { id: 501,
+      image: 'assets/images/screenshots/vs-event.jpg',
+      caption: "Event page offers interactive seating chart and detailed ticket information."
+    },
+    { id: 502,
+      image: 'assets/images/screenshots/vs-login.jpg',
+      caption: "My accounts allows users to check purchase status, sales status, and newsletter settings."
     },
     { id: 600,
-      title: ' ',
-      image: 'assets/images/wires/hscnews_guide2.jpg'
+      image: 'assets/images/screenshots/hsc-splash.jpg',
+      caption: "Count down page prior to site release."
     },
-    { id: 700,
-      title: ' ',
-      image: 'assets/images/wires/hscnews_guide3.jpg'
+    { id: 601,
+      image: 'assets/images/screenshots/hsc-home.jpg',
+      caption: "Season Pass showcased high school sports offering stats, highlights and schedules."
     },
-    { id: 800,
-      title: ' ',
-      image: 'assets/images/wires/hscnews_guide4.jpg'
+    { id: 602,
+      image: 'assets/images/screenshots/hsc-guide.jpg',
+      caption: "Style guides allowed us to inventory each widget we included on the site. This included markdown for interns to plug into the system for ease of use."
     },
   ]
 });
