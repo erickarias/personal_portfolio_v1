9b29171effceaa123d71d0792a772a80082c40b0
Erick Arias <erick.arias@wrapports.com>
2014-09-27 13:17:38 -0500

    adjusted mobile proj navigation

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 	this.resource('project', { path: '/:project_id'});
 	this.resource('resume', { path: 'resume'});
+	this.resource('about', { path: 'about'});
 });
 
 export default Router;
